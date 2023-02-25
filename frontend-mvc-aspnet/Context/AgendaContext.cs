1d1b878f490d7b096693e0b46c1e3c9d6ecea17f
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-02-24 21:50:00 -0300

    Configurando DbContext

---
+++ frontend-mvc-aspnet/Context/AgendaContext.cs
@@ -0,0 +1,13 @@
+﻿using frontend_mvc_aspnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace frontend_mvc_aspnet.Context;
+
+public class AgendaContext : DbContext
+{
+    public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Contato> Contatos { get; set; }
+}
