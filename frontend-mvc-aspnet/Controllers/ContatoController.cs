6c07b8584cc3b0641a09b9a8ce1f65c29cf6351d
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-02-26 21:33:44 -0300

    Criando página de Contato

---
+++ frontend-mvc-aspnet/Controllers/ContatoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace frontend_mvc_aspnet.Controllers;
+
+public class ContatoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
