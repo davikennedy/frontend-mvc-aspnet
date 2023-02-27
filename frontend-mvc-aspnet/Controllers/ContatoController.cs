3229c219057db8dcb5ade8e420487cecf8b8f291
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-02-27 09:52:52 -0300

    Configurando ContatoController

--- frontend-mvc-aspnet/Controllers/ContatoController.cs
+++ frontend-mvc-aspnet/Controllers/ContatoController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using frontend_mvc_aspnet.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace frontend_mvc_aspnet.Controllers;
 
 public class ContatoController : Controller
 {
+    private readonly AgendaContext _context;
+
+    public ContatoController(AgendaContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
-        return View();
+        var contatos = _context.Contatos.ToList();
+        return View(contatos);
     }
 }
